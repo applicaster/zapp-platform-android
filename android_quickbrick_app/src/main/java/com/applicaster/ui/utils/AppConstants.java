0d089c7f8bb264a189ad2546ae8527b6545352b7
Anna Bauza <31966073+annabauza@users.noreply.github.com>
2020-08-04 14:19:44 +0100

    feat: Zapp app missing analytics values (#53)

--- android_quickbrick_app/src/main/java/com/applicaster/ui/utils/AppConstants.java
+++ android_quickbrick_app/src/main/java/com/applicaster/ui/utils/AppConstants.java
@@ -27,6 +27,8 @@ public class AppConstants {
     private static final String INITIAL_PROPS = "initialProps";
     private static final String API_SECRET_KEY = "apiSecretKey";
     private static final String ZAPP_VERSION = "versionName";
+    private static final String DEVICE_ID = "deviceId";
+    private static final String RIVERS_ID = "riversConfigurationId";
 
     public static Map<String, Object> generateMap(Context context) {
         Map<String, Object> constants = new HashMap<>();
@@ -37,6 +39,8 @@ public class AppConstants {
         constants.put(INITIAL_PROPS,        InitialProperties.getBundle(context));
         constants.put(API_SECRET_KEY,       AppData.getProperty(APProperties.API_KEY));
         constants.put(ZAPP_VERSION,         OSUtil.getZappAppVersion());
+        constants.put(DEVICE_ID,            OSUtil.getDeviceIdentifier(context));
+        constants.put(RIVERS_ID,            AppData.getProperty(RIVERS_ID));
         return constants;
     }
 }
