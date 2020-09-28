3cda5f69ef3a0409412a9f50f53f02b591740739
Alex <24409942+inckie@users.noreply.github.com>
2020-09-28 15:03:02 -0400

    Feat: simplify and generalize PreloadStateManager

--- android_quickbrick_app/src/main/java/com/applicaster/ui/loaders/PreloadStateManager.kt
+++ android_quickbrick_app/src/main/java/com/applicaster/ui/loaders/PreloadStateManager.kt
@@ -1,42 +1,34 @@
-package com.applicaster.ui.loaders;
+package com.applicaster.ui.loaders
 
-import android.util.Log;
+import android.util.Log
 
 /**
- * Does nothing except holding multiple boolean flags that are marked as steps to complete.
- * If all flags are set as true, the status is considered as complete - {@link #isPreloadComplete()}
+ * Does nothing except holding multiple steps to complete.
+ * If no steps left, preload considered complete - [.isPreloadComplete]
  */
-public class PreloadStateManager {
-    public enum PreloadStep {
-        LOAD_DATA,
-        VIDEO_INTRO,
+class PreloadStateManager {
+
+    enum class PreloadStep {
+        LOAD_DATA, VIDEO_INTRO
     }
-    private final String TAG = getClass().getSimpleName();
-    private boolean loadDataComplete;
-    private boolean videoIntroComplete;
+
+    private val TAG = javaClass.simpleName
+    private val steps = hashSetOf(*PreloadStep.values())
 
     /**
-     * Set a {@link PreloadStep} flag to true.
+     * Complete [PreloadStep].
      * @param step  Preload step to marked as complete
      */
-    public synchronized void setStepComplete(PreloadStep step) {
-        Log.d(TAG, "Preload step complete: " + step);
-        switch (step) {
-            case LOAD_DATA:
-                loadDataComplete = true;
-                break;
-            case VIDEO_INTRO:
-                videoIntroComplete = true;
-                break;
-            default:
-                break;
-        }
+    @Synchronized
+    fun setStepComplete(step: PreloadStep) {
+        Log.d(TAG, "Preload step complete: $step")
+        steps.remove(step)
     }
 
     /**
      * @return Are all the preload steps marked as ready/complete?
      */
-    public boolean isPreloadComplete() {
-        return loadDataComplete && videoIntroComplete;
-    }
+    val isPreloadComplete: Boolean
+        @Synchronized
+        get() = steps.isEmpty()
 }
