6152d62cb3a00908f609225663bac43bdcdc2234
aleksey.kutsko <kutsko@scand.com>
2021-03-31 12:01:54 +0300

    fix: TV RTL direction.

--- android_quickbrick_app/src/main/java/com/applicaster/ui/quickbrick/QuickBrickManager.java
+++ android_quickbrick_app/src/main/java/com/applicaster/ui/quickbrick/QuickBrickManager.java
@@ -42,9 +42,6 @@ import java.util.List;
 
 import okhttp3.OkHttpClient;
 
-import android.app.UiModeManager;
-import android.content.res.Configuration;
-
 public class QuickBrickManager implements
         IUILayerManager,
         DefaultHardwareBackBtnHandler,
@@ -453,28 +450,21 @@ public class QuickBrickManager implements
                 .emit(RN_EVENT_HANDLE_OPEN_URL, resultMap);
     }
 
-    /**
-     * Check if app runs on TV platform.
-     **/
-    private boolean isTVPlatform() {
-        UiModeManager uiModeManager = (UiModeManager) this.rootActivity.getSystemService(Context.UI_MODE_SERVICE);
-        return uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION;
-    }
-
     public void setRightToLeftFlag() {
-        List<String> languageList = AppData.getAvailableLocalizations();
-        String appLocale = AppData.getLocale().toString();
-        String localeToUse = languageList.isEmpty() || languageList.contains(appLocale)
-                ? appLocale : languageList.get(0);
-
-        if(isTVPlatform()) {
+        if (OSUtil.isTv()) {
             I18nUtil.getInstance().allowRTL(this.rootActivity, false);
-        }
+            I18nUtil.getInstance().forceRTL(this.rootActivity, false);
+        } else {
+            List<String> languageList = AppData.getAvailableLocalizations();
+            String appLocale = AppData.getLocale().toString();
+            String localeToUse = languageList.isEmpty() || languageList.contains(appLocale)
+                    ? appLocale : languageList.get(0);
 
-        I18nUtil.getInstance().forceRTL(
-                this.rootActivity,
-                RTL_LOCALES.includes(localeToUse)
-        );
+            I18nUtil.getInstance().forceRTL(
+                    this.rootActivity,
+                    RTL_LOCALES.includes(localeToUse)
+            );
+        }
     }
 
     @Override
